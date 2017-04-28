5a0c6d9330f6f07d5a44a0bb6763e5f62819e353
Kazinczi Márton <kazinczi@gmail.com>
2017-04-28 11:54:56 +0200

    ItemList module added the missing functionality to the presenter and scree.

--- app/src/main/java/hu/bm/mobsoft/lab/costlog/MobSoftApplicationComponent.java
+++ app/src/main/java/hu/bm/mobsoft/lab/costlog/MobSoftApplicationComponent.java
@@ -11,6 +11,7 @@ import hu.bm.mobsoft.lab.costlog.ui.UIModule;
 import hu.bm.mobsoft.lab.costlog.ui.createitem.CreateItemActivity;
 import hu.bm.mobsoft.lab.costlog.ui.createitem.CreateItemPresenter;
 import hu.bm.mobsoft.lab.costlog.ui.itemlist.ItemListActivity;
+import hu.bm.mobsoft.lab.costlog.ui.itemlist.ItemListPresenter;
 import hu.bm.mobsoft.lab.costlog.ui.loing.LoginActivity;
 import hu.bm.mobsoft.lab.costlog.ui.main.MainActivity;
 import hu.bm.mobsoft.lab.costlog.ui.main.MainPresenter;
@@ -20,19 +21,18 @@ import hu.bm.mobsoft.lab.costlog.ui.statistic.StatisticActivity;
 @Singleton
 @Component(modules = {UIModule.class, RepositoryModule.class, InteractorModule.class})
 public interface MobSoftApplicationComponent {
-    void inject(MainActivity        mainActivity);
-    void inject(LoginActivity       loginActivity);
-    void inject(ItemListActivity    itemListActivity);
-    void inject(CreateItemActivity  createItemActivity);
+    void inject(MainActivity mainActivity);
+    void inject(LoginActivity loginActivity);
+    void inject(ItemListActivity itemListActivity);
+    void inject(CreateItemActivity createItemActivity);
     void inject(ModifieItemActivity modifieItemActivity);
-    void inject(StatisticActivity   statisticActivity);
-
+    void inject(StatisticActivity statisticActivity);
 
     void inject(CostItemInteractor costItemInteractor);
     void inject(UserInteractor userInteractor);
 
-
     void inject(MainPresenter mainPresenter);
+    void inject(ItemListPresenter itemListPresenter);
 
 
 }
--- app/src/main/java/hu/bm/mobsoft/lab/costlog/ui/itemlist/ItemListScreen.java
+++ app/src/main/java/hu/bm/mobsoft/lab/costlog/ui/itemlist/ItemListScreen.java
@@ -1,6 +1,13 @@
 package hu.bm.mobsoft.lab.costlog.ui.itemlist;
 
 
+import java.util.List;
+
+import hu.bm.mobsoft.lab.costlog.model.Item;
+
 public interface ItemListScreen {
+    void showErrorMessage(String message);
     void showList();
+
+    void listItems(List<Item> items);
 }
